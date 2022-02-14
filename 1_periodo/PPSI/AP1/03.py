c1fc1d5e02b1c61ccc2ed37953e178ca135a1ad0
navxga <lyannavega@gmail.com>
2022-02-14 14:45:23 -0300

    s

--- 1_periodo/PPSI/AP1/03.py
+++ 1_periodo/PPSI/AP1/03.py
@@ -1,5 +1,9 @@
 print('Calcule seu peso!')
 h = float(input('Digite a sua altura, em metros: '))
-ph = (72.7 * h) - 58
-pm = (62.1 * h) - 44.7
-print(f'Se você for homem, seu peso ideal é: {ph:.1f}kg. Se for mulher: {pm:.1f}kg')
+sx = input('Você é homem ou mulher? h/m: ')
+if(sx == 'h'):
+    peso = (72.7 * h) - 58
+    print(f'O seu peso ideal é {peso:.2f}kg')
+else:
+    peso = (62.1 * h) - 44.7
+    print(f'O seu peso ideal é {peso:.2f}kg')
---
+++ 1_periodo/PPSI/AP1/08.py
@@ -0,0 +1,16 @@
+notas = [10, 15, 20, 25]
+
+if notas < [-1] :
+    print('Error!!')
+else:
+    # Quantidade de valores
+    qv = print(len(notas))
+
+    # Valores na ordem
+    print(notas)
+
+    # Valores na ordem inversa
+    print()
+
+    # Soma
+    print(notas[qv])
