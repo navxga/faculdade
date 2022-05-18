1e1a71f49b034e531be199444200fae4967180af
navxga <lyannavega@gmail.com>
2022-05-18 14:55:35 -0300

    final do CAS

--- 1_periodo/CAS/M00_Codes/ap16/src/App.java
+++ 1_periodo/CAS/M00_Codes/ap16/src/App.java
@@ -1,5 +1,29 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import javax.swing.*;
+
+class App 
+{
+    public static void soma (int vet[]) {
+        int s = 0;
+        for (int i = 0; i < vet.length; i++) {
+            s += vet[i];
+        }
+        JOptionPane.showMessageDialog(null, "A somatória é "+s);
+    }
+    
+    public static int produto (int vet[]) {
+        int p = 1;
+        for (int i = 0; i < vet.length; i++) {
+            p *= vet[i];
+        }
+        return p;
+    }
+    
+    public static void main(String entrada[]) {
+        int r, s = 0, vetor[] = {2, 4, 6, 8, 10};
+
+        soma(vetor);
+        r = produto(vetor);
+        JOptionPane.showMessageDialog(null, "A produtória é "+r);
+        System.exit(0);
     }
 }
